f4b8a2c60fb265574394c1da973f584a104aee2d
luojun <jun@dragonflystudios.ca>
2014-07-13 13:18:30 -0400

    Initial Commit

---
+++ RandomUser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RandomUser
+//
+//  Created by Jun Luo on 2014-07-13.
+//  Copyright (c) 2014 Jun Luo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RandomUser/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  RandomUser
+//
+//  Created by Jun Luo on 2014-07-13.
+//  Copyright (c) 2014 Jun Luo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController <UISplitViewControllerDelegate>
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
