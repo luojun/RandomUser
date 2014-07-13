46e0014d2c3b5fd3f3610aced868bb1ae21840f5
luojun <jun@dragonflystudios.ca>
2014-07-13 14:28:39 -0400

    add model and corresponding CoreData initialization call

---
+++ RandomUser/Model/RandomUserModelConfig.h
@@ -0,0 +1,18 @@
+//
+//  RandomUserModelConfig.h
+//  RandomUser
+//
+//  Created by Jun Luo on 2014-07-13.
+//  Copyright (c) 2014 Jun Luo. All rights reserved.
+//
+
+#define kRandomUserModel @"RandomUser"
+#define kRandomUserStore @"RandomUser.sqlite"
+
+#define kRUMAddress @"address"
+#define kRUMCell @"cell"
+#define kRUMEmail @"email"
+#define kRUMGender @"gender"
+#define kRUMName @"name"
+#define kRUMPicture @"picture"
+#define kRUMTitle @"registered"
